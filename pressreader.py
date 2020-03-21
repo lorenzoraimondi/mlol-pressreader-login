8d5be16c93d95cb60ec74c1a2cec2f11d3947266
Lorenzo <lorenzo.raimondi23@gmail.com>
2020-03-21 15:43:33 +0100

    Update pressreader.py

--- pressreader.py
+++ pressreader.py
@@ -1,11 +1,8 @@
 import os
-import sys
 import time
 from utilities import log, build_driver
 
 log("Pressreader script start...")
-print("start")
-sys.stdout.flush()
 
 MLOL_USR = os.environ['MLOL_USR']
 MLOL_PSW = os.environ['MLOL_PSW']
@@ -31,12 +28,8 @@ try:
     # Moving to Pressreader
     log("Opening Pressreader...")
     driver.get("https://csbno.medialibrary.it/media/view.aspx?id=550276273")
-    #time.sleep(10)
-    #driver.find_element_by_link_text("SFOGLIA").click()
     log("Connected to Pressreader")
-    #time.sleep(10)
-    #driver.switch_to_window(driver.window_handles[1])
-    #log("Switching window...")
+
     try:
         log("Closing Alert...")
         driver.find_element_by_class_name("alert-close").click()
@@ -45,16 +38,16 @@ try:
         log(e)
     
     # Pressreader Logging in 
-    log("Logging in Pressreader")
+    log("Logging in Pressreader...")
     driver.find_element_by_xpath("//div[@id='toolbarTop']/div/div[3]/a/span/span[2]").click()
     driver.find_element_by_id("SignInEmailAddress").send_keys(PR_USR)
     driver.find_element_by_xpath("//input[@type='password']").send_keys(PR_PSW)
     driver.find_element_by_xpath("//button[@type='submit']").click()
-    log("Logged in Pressreader")
+    log("Logged in!")
     
     driver.close()
-    log("Success! Exiting...")
+    log("Success!")
 except Exception as e:
     log("Exception occurred")
     log(e)
-    log("Execution Failed! Exiting...")
+    log("Execution Failed!")
