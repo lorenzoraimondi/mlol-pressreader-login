045833b0a7cac3c5dbc95195a6f4e17bc8c7d0eb
Lorenzo <lorenzo.raimondi23@gmail.com>
2020-03-21 17:13:08 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,9 +3,9 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 cron_schedule = "0 0 */6 * *" # every 6 days
-debug_schedule = "*/3 * * * *" # every 3 minutes
+debug_cron_schedule = "*/3 * * * *" # every 3 minutes
 
 scheduler = BlockingScheduler()
-scheduler.add_job(job, CronTrigger.from_crontab(debug_schedule))
+scheduler.add_job(job, CronTrigger.from_crontab(cron_schedule))
 
 scheduler.start()
