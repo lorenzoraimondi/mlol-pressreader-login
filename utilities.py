3f02bbccf8261c3ab1e538ea92170c4b911f5fdf
Lorenzo <lorenzo.raimondi23@gmail.com>
2020-03-21 13:37:44 +0100

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -7,14 +7,13 @@ def log(string):
     print("[{}] {}".format(t, string))
     
 def build_driver():
-    GOOGLE_CHROME_PATH = os.environ["GOOGLE_CHROME_BIN"]
+    GOOGLE_CHROME_SHIM = os.environ["GOOGLE_CHROME_SHIM"]
+    log("GOOGLE_CHROME_SHIM > {}".format(GOOGLE_CHROME_SHIM))
     CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
     WAIT = 10
     
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
+    chrome_options.binary_location = GOOGLE_CHROME_SHIM
     #driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.implicitly_wait(WAIT)
